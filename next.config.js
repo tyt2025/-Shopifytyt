8e79eb763b84e56aedc7e038800c69b259564779
tyt2025 <tintasytecnologia2019@gmail.com>
2025-11-13 09:34:29 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['cdn.shopify.com', 'cxxifwpwarbrrodtzyqn.supabase.co'],
-  },
-}
-
-module.exports = nextConfig
